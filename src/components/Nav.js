3dfea453b1e7b57577d992e0d6ebd59062a7f275
Carlos E Alford M <carlos.alford@gmail.com>
2020-07-19 20:06:10 +0100

    feat: Nav component, ready, basic

---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+export default function Nav(props) {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <NavLink to="/" exact activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Create Poll" activeClassName="active">
+            Create Poll
+          </NavLink>
+        </li>
+          <NavLink to="/leaderboard" activeClassName="active">
+            Leaderboard
+          </NavLink>
+        <li>
+        </li>
+      </ul>
+    </nav>
+  )
+}
