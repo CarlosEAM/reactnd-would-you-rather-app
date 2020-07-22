5209eee8d418bc80b7646574c8d1ca08c2a04fc1
Carlos E Alford M <carlos.alford@gmail.com>
2020-07-22 23:19:42 +0100

    Create PollDetails component to handle the /questions/:question_id page

--- src/components/PollDetail.js
+++ src/components/PollDetail.js
@@ -1,85 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
-import { handleUpdateQuestion } from '../actions/questions'
-import { updateUserAnswer } from '../actions/users'
 
 // Custom components
-import PollHeader from './PollHeader'
+import PollAnswer from './PollAnswer'
+import PollQuestion from './PollQuestion'
 
 
 // Container component
-class AnswerPoll extends Component {
-  state = {
-    answer: '',
-  }
+function AnswerPoll(props) {
+  const { question_id, pollType } = props;
 
-  handleChange = (e) => {
-    this.setState({
-      answer: e.target.value,
-    });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const { answer } = this.state;
-    const { dispatch, authedUser } = this.props;
-    const { id } = this.props.question;
-
-    // Update the store with answer
-    dispatch(handleUpdateQuestion(id, answer))
-    // Update current user with their answer
-    dispatch(updateUserAnswer(authedUser, id, answer))
-  }
-
-  render() {
-    const { answer } = this.state;
-    const disabled = answer === '' ? true : false;
-    return (
-      <div className="answer-poll">
-        <PollHeader authorID={this.props.question.author} />
-        <form className="form" onSubmit={this.handleSubmit}>
-          <label className="input-radio__label">
-            <input
-              className="input-radio"
-              type="radio"
-              name="poll questions"
-              value="optionOne"
-              onChange={this.handleChange}
-            />
-            <span className="input-radio__label-text">{this.props.question.optionOne.text}</span>
-          </label>
-          <p>or</p>
-          <label className="input-radio__label">
-            <input
-              className="input-radio"
-              type="radio"
-              name="poll questions"
-              value="optionTwo"
-              onChange={this.handleChange}
-            />
-            <span className="input-radio__label-text">{this.props.question.optionTwo.text}</span>
-          </label>
-          <button
-            className="submit-btn"
-            type="submit"
-            disabled={disabled}
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className="poll-details">
+      {(pollType === 'answer')
+        ? <PollAnswer qid={question_id} />
+        : <PollQuestion qid={question_id} />}
+    </div>
+  )
 }
 
-function mapStateToProps({ authedUser, users, questions }, props) {
+function mapStateToProps({ questions, users, authedUser }, props) {
   const { question_id } = props.match.params;
+  const options = ['optionOne', 'optionTwo'];
+  let pollType = 'question';
 
-  const question = questions[question_id]
+  // Check which poll type we looking to display
+  // Use 'pollType = answer | false' to inform type of poll to render
+  options.forEach(option => {
+    if (pollType === 'question' && questions[question_id][option].votes.includes(authedUser)) {
+      pollType = 'answer'
+    }
+  });
+
   return {
-    authedUser,
-    question,
+    question_id,
+    pollType,
   }
 }
 
