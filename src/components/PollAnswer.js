17ad7d47ba40f25baf20881d4e60309456b1a503
Carlos E Alford M <carlos.alford@gmail.com>
2020-08-08 12:19:11 +0100

    style: Complete CSS for Poll Details

--- src/components/PollAnswer.js
+++ src/components/PollAnswer.js
@@ -7,26 +7,29 @@ function PollAnswer(props) {
 
   return (
     <div className="poll-answer">
-      {props.options.map((option, i) => (
-        <div key={option.answer} className="answer-option">
-         {(option.answer === chosenAnswer || option.answer === chosenAnswer)
-          ? (<div className="answer-option--item answer-option__avatar">
-              <img src={avatar} alt="" />
-            </div>)
-          : null}
-          <div className="answer-option--item answer-option__text">
-            {option.text}
+      <h2 className="poll-answer__header">Answer Details</h2>
+      <div className="poll-answer__wrapper">
+        {props.options.map((option, i) => (
+          <div key={option.answer} className="answer-option">
+           {(option.answer === chosenAnswer || option.answer === chosenAnswer)
+            ? (<div className="answer-option--item answer-option__avatar">
+                <img src={avatar} alt="" />
+              </div>)
+            : null}
+            <div className="answer-option--item answer-option__text">
+              {option.text}
+            </div>
+            <div className="answer-option--item answer-option__vote-total">
+              <p>Number Of Votes</p>
+              <span>{option.nOfVotes} of {totalVotes}</span>
+            </div>
+            <div className="answer-option--item answer-option__vote-percentage">
+              <p>{option.percentOfVotes}%</p>
+            </div>
+            <div className="answer-option--item"></div>
           </div>
-          <div className="answer-option--item answer-option__vote-total">
-            <p>Number Of Votes</p>
-            <span>{option.nOfVotes} of {totalVotes}</span>
-          </div>
-          <div className="answer-option--item answer-option__vote-percentage">
-            <p>{option.percentOfVotes}%</p>
-          </div>
-          <div className="answer-option--item"></div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
