07b4902c6437e2fc4139d2152aafc3b24c4b4bd2
Carlos E Alford M <carlos.alford@gmail.com>
2020-08-13 21:55:45 +0100

    feat: Create 404 Page component

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Avatar_404page from '../images/Avatar_404page.png'
 
 export default function Page404(props) {
   return (
-    <p>404 PAGE</p>
+    <div className="page404">
+      <h1>404 PAGE</h1>
+      <div className="page404__avatar">
+        <img src={Avatar_404page} alt="avatar" />
+      </div>
+      <p>The page you are trying to view does not exist</p>
+    </div>
   )
 }
